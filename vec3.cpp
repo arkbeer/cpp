d6612a3c4e710eb65841264221c37f0d40466d28
Y.Y <ArkBeer@users.noreply.github.com>
2018-09-18 12:58:51 +0900

    Update vec3.cpp

--- vec3.cpp
+++ vec3.cpp
@@ -14,21 +14,63 @@ class vec3{
     const auto b(){return e.at(2);}
     const vec3& operator+(){return *this;}
     const vec3 operator-(){
-        return vec3(std::array<double,3>{-e.at(0),-e.at(1),-e.at(2)});
+        return vec3{{-e.at(0),-e.at(1),-e.at(2)}};
     }
     double operator[](int i) const{return e.at(i);}
     double& operator[](int i){return e.at(i);}
         
-    vec3& operator+=(const vec3& v2);
-    vec3& operator-=(const vec3& v2);
-    vec3& operator*=(const vec3& v2);
-    vec3& operator/=(const vec3& v2);
-    vec3& operator*=(const double t);
-    vec3& operator/=(const double t);
+    vec3& operator+=(const vec3& v2){
+        this->e.at(0)+=v2.e.at(0);
+        this->e.at(1)+=v2.e.at(1);
+        this->e.at(2)+=v2.e.at(2);
+        return *this;
+    }
+    vec3& operator-=(const vec3& v2){
+        this->e.at(0)-=v2.e.at(0);
+        this->e.at(1)-=v2.e.at(1);
+        this->e.at(2)-=v2.e.at(2);
+        return *this;
+    }
+    vec3& operator*=(const vec3& v2){
+        this->e.at(0)*=v2.e.at(0);
+        this->e.at(1)*=v2.e.at(1);
+        this->e.at(2)*=v2.e.at(2);
+        return *this;
+    }
+    vec3& operator/=(const vec3& v2){
+        this->e.at(0)/=v2.e.at(0);
+        this->e.at(1)/=v2.e.at(1);
+        this->e.at(2)/=v2.e.at(2);
+        return *this;
+    }
+    vec3& operator*=(const double t){
+        this->e.at(0)*=t;
+        this->e.at(1)*=t;
+        this->e.at(2)*=t;
+        return *this;
+    }
+    vec3& operator/=(const double t){
+        this->e.at(0)/=t;
+        this->e.at(1)/=t;
+        this->e.at(2)/=t;
+        return *this;
+    }
     friend std::ostream& operator<<(std::ostream&, const vec3&);
     friend std::istream& operator>>(std::istream&, vec3&);
+    friend vec3 operator+(const vec3&, const vec3&);
+    friend vec3 operator-(const vec3&, const vec3&);
+    friend vec3 operator*(const vec3&, const vec3&);
+    friend vec3 operator/(const vec3&, const vec3&);
+    friend vec3 operator*(const vec3&, const double);
+    friend vec3 operator/(const vec3&, const double);
+    double square_length()const{
+        return e.at(0)*e.at(0)+e.at(1)*e.at(1)+e.at(2)*e.at(2);
+    }
     double length()const {
-        return std::sqrt(e.at(0)*e.at(0)+e.at(1)*e.at(1)+e.at(2)*e.at(2));
+        return std::sqrt(square_length());
+    }
+    vec3 unit(){
+        return *this/length();
     }
 };
 std::ostream& operator<<(std::ostream& os, const vec3& v){
@@ -38,6 +80,24 @@ std::ostream& operator<<(std::ostream& os, const vec3& v){
 std::istream& operator>>(std::istream& is, vec3& v){
     is>>v.e.at(0)>>v.e.at(1)>>v.e.at(2);
     return is;
+}
+vec3 operator+(const vec3& v1, const vec3& v2){
+    return vec3{{v1.e.at(0)+v2.e.at(0),v1.e.at(1)+v2.e.at(1),v1.e.at(2)+v2.e.at(2)}};
+}
+vec3 operator-(const vec3& v1, const vec3& v2){
+    return vec3{{v1.e.at(0)-v2.e.at(0),v1.e.at(1)-v2.e.at(1),v1.e.at(2)-v2.e.at(2)}};
+}
+vec3 operator*(const vec3& v1, const vec3& v2){
+    return vec3{{v1.e.at(0)*v2.e.at(0),v1.e.at(1)*v2.e.at(1),v1.e.at(2)*v2.e.at(2)}};
+}
+vec3 operator/(const vec3& v1, const vec3& v2){
+    return vec3{{v1.e.at(0)/v2.e.at(0),v1.e.at(1)/v2.e.at(1),v1.e.at(2)/v2.e.at(2)}};
+}
+vec3 operator*(const vec3& v1, const double t){
+    return vec3{{v1.e.at(0)*t,v1.e.at(1)*t,v1.e.at(2)*t}};
+}
+vec3 operator/(const vec3& v1, const double t){
+    return vec3{{v1.e.at(0)/t,v1.e.at(1)/t,v1.e.at(2)/t}};
 }
 int main(){
     vec3 v{{1,1,1}};
