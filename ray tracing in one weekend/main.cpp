148bcba76da82f363f88f8e401eb4b374108ba1c
Y.Y <3901776+arkbeer@users.noreply.github.com>
2018-10-23 12:12:46 +0900

    Update main.cpp

--- ray tracing in one weekend/main.cpp
+++ ray tracing in one weekend/main.cpp
@@ -3,7 +3,16 @@ std::ostream& make_header(std::ostream& ost,const int nx,const int ny,const int 
     ost<<"P3\n"<<nx<<" "<<ny<<"\n"<<ncolor<<std::endl;
     return ost;
 }
+const int nx=200,ny=100;
 int main(){
     make_header(std::cout,200,100,255);
+    for(int j=ny-1;j>=0;--j){
+        for(int i=0;i<nx;++i){
+            vec3 col{{static_cast<double>(i)/nx,static_cast<double>(j)/ny,0.2}};
+            std::cout<<col<<std::endl;
+            
+        }
+    }
+    
     return 0;
 }
