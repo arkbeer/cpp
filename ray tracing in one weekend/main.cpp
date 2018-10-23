cc3e95b56bc223d6e0031b56bc989ce8df171545
Y.Y <3901776+arkbeer@users.noreply.github.com>
2018-10-23 12:50:57 +0900

    Update main.cpp

--- ray tracing in one weekend/main.cpp
+++ ray tracing in one weekend/main.cpp
@@ -1,18 +1,21 @@
 #include"vec3.hpp"
+#include<fstream>
 std::ostream& make_header(std::ostream& ost,const int nx,const int ny,const int ncolor){
     ost<<"P3\n"<<nx<<" "<<ny<<"\n"<<ncolor<<std::endl;
     return ost;
 }
 const int nx=200,ny=100;
 int main(){
-    make_header(std::cout,200,100,255);
+    std::ofstream ofs("image.ppm");
+    
+    make_header(ofs,200,100,255);
     for(int j=ny-1;j>=0;--j){
         for(int i=0;i<nx;++i){
             vec3 col{{static_cast<double>(i)/nx,static_cast<double>(j)/ny,0.2}};
-            std::cout<<col<<std::endl;
-            
+            ofs<<col<<std::endl;
         }
     }
+    ofs.close();
     
     return 0;
 }
