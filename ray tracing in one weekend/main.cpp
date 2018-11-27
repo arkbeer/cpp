aa521e71be46e04a78cd7423e17f965cf135760b
Y.Y <3901776+arkbeer@users.noreply.github.com>
2018-11-28 04:10:38 +0900

    Add files via upload

--- ray tracing in one weekend/main.cpp
+++ ray tracing in one weekend/main.cpp
@@ -1,19 +1,32 @@
 #include"vec3.hpp"
 #include"ray.hpp"
 #include<fstream>
-const bool hit_sphere(const vec3& center, const double radius, const ray& r){
+const double hit_sphere(const vec3& center, const double radius, const ray& r){
     const vec3 oc=r.origin()-center;
     const double a=dot(r.direction(),r.direction());
     const double b=2.0*dot(oc,r.direction());
     const double c=dot(oc,oc)-radius*radius;
     const double discriminant=b*b-4*a*c;
-    return (discriminant>0);
+    //return (discriminant>0);
+	if (discriminant < 0) {
+		return -1;
+	}
+	else {
+		return (-b - std::sqrtf(discriminant)) / (2.0*a);
+	}
+
 }
 const vec3 color(const ray& r){
-    if(hit_sphere(vec3{{0,0,-1}},0.5,r))return vec3{{1,0,0}};
+	vec3 c{ {0,0,-1} };
+	double t = hit_sphere(c, 0.5f, r);
+
+	if (t > 0) {
+		vec3 n = (r.point_at_param(t) - c).unit();
+		return 0.5*(n + vec3{ {1.0,1.0,1.0} });
+	}
     vec3 unit_direction=r.direction().unit();
-    double t=0.5*(unit_direction.y()+1.0);
-    return (1.0-t)*vec3{{1.0,1.0,1.0}}+t*vec3{{0.5,0.7,1.0}};
+    t=0.5*(unit_direction.y()+1.0);
+    return (1-t)*vec3{{1.0,1.0,1.0}}+t*vec3{{0.5,0.7,1.0}};
 }
 std::ostream& make_header(std::ostream& ost,const int nx,const int ny,const int ncolor){
     ost<<"P3\n"<<nx<<" "<<ny<<"\n"<<ncolor<<std::endl;
--- ray tracing in one weekend/ray.hpp
+++ ray tracing in one weekend/ray.hpp
@@ -1,11 +1,12 @@
+#pragma once
 #include"vec3.hpp"
 
-class ray{
-    vec3 A,B;
-    public:
-    ray(const vec3&a, const vec3& b):A(a),B(b){}
-    ray(){}
-    const auto origin() const {return A;}
-    const auto direction() const {return B;}
-    const auto point_at_param(const double t) const {return A+t*B;}
+class ray {
+	vec3 A, B;
+public:
+	ray(const vec3&a, const vec3& b) :A(a), B(b) {}
+	ray() {}
+	const auto origin() const { return A; }
+	const auto direction() const { return B; }
+	const auto point_at_param(const double t) const { return A + t * B; }
 };
