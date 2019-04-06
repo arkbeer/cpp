d96db10640c3bea57f23444ca69c78b975700e68
Y.Y <3901776+arkbeer@users.noreply.github.com>
2019-04-06 15:21:12 +0900

    Add files via upload

---
+++ rayt/one weekend/Chapter 7/sphere.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include"hitable.hpp"
+#include<complex>
+namespace rayt{
+    class sphere :public hitable{
+        vec3 _center;
+        float _radius;
+    public:
+        sphere(const vec3& center, const float radius):_center(center), _radius(radius){}
+        sphere():sphere(vec3(),1){}
+        virtual bool hit(const ray& r, const float t_min, const float t_max, hitinfo& info) const;
+    };
+    bool sphere::hit(const ray& r, const float t_min, const float t_max, hitinfo& info)const {
+        rayt::vec3 oc=r.origin()-_center;
+        const float a=dot(r.direction(), r.direction());
+        const float b=dot(r.direction(), oc);
+        const float c=dot(oc, oc)-_radius*_radius;
+        const float d=b*b-a*c;
+        if(d>0){
+            float t=(-b-std::sqrt(d))/a;
+            if(t_min<t && t<t_max){
+                info.t=t;
+                info.p=r.point_at_parameter(info.t);
+                info.normal=(info.p-_center)/_radius;
+                return true;
+            }
+            t=(-b+std::sqrt(d))/a;
+            if(t_min<t && t<t_max){
+                info.t=t;
+                info.p=r.point_at_parameter(info.t);
+                info.normal=(info.p-_center)/_radius;
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ rayt/one weekend/Chapter 7/vec3.hpp
@@ -0,0 +1,63 @@
+#pragma once
+namespace rayt{
+    class vec3{
+        float _x,_y,_z;
+        public:
+        vec3(const float x, const float y, const float z):_x(x),_y(y),_z(z){}
+        vec3():vec3(0,0,0){}
+        ~vec3(){}
+                const float square()const{
+            return _x*_x+_y*_y+_z*_z;
+        }
+        const float length()const{
+            return std::sqrt(square());
+        }
+        const vec3 unit()const{
+            return *this/length();
+        }
+#define FUNC(TYPE, NAME) const TYPE NAME()const{return _##NAME;}
+        FUNC(float, x);
+        FUNC(float, y);
+        FUNC(float, z);
+#undef FUNC
+#define FUNC(TYPE, NAME, COORD) const TYPE NAME()const{return static_cast<int>(_##COORD*255.99);}
+        FUNC(int, r, x);
+        FUNC(int, g, y);
+        FUNC(int, b, z);
+#undef FUNC
+#define OPERATOR(OPENAME) vec3& operator OPENAME(const vec3& v){_x OPENAME v.x();_y OPENAME v.y();_z OPENAME v.z();return *this;}
+        OPERATOR(+=)
+        OPERATOR(-=)
+#undef OPERATOR
+#define OPERATOR(OPENAME) vec3& operator OPENAME(const float t){_x OPENAME t;_y OPENAME t;_z OPENAME t;return *this;}
+        OPERATOR(*=)
+        OPERATOR(/=)
+#undef OPERATOR
+#define FRIEND_OPERATOR(OPENAME) friend vec3 operator OPENAME(const vec3&, const vec3&);
+        FRIEND_OPERATOR(+)
+        FRIEND_OPERATOR(-)
+        FRIEND_OPERATOR(*)
+        FRIEND_OPERATOR(/)
+#undef FRIEND_OPERATOR
+#define FRIEND_OPERATOR(OPENAME) friend vec3 operator OPENAME(const vec3&, const float);
+        FRIEND_OPERATOR(*)
+        FRIEND_OPERATOR(/)
+#undef FRIEND_OPERATOR
+    };
+const float dot(const vec3& v1, const vec3& v2){
+    return v1.x()*v2.x()+v1.y()*v2.y()+v1.z()*v2.z();
+}
+#define OPERATOR(OPENAME) vec3 operator OPENAME(const vec3& v1, const vec3& v2){return vec3(v1.x() OPENAME v2.x(), v1.y() OPENAME v2.y(), v1.z() OPENAME v2.z());}
+    OPERATOR(+)
+    OPERATOR(-)
+    OPERATOR(*)
+    OPERATOR(/)
+#undef OPERATOR
+#define OPERATOR(OPENAME) vec3 operator OPENAME(const vec3& v, const float t){return vec3(v.x() OPENAME t, v.y() OPENAME t, v.z() OPENAME t);}
+    OPERATOR(*)
+    OPERATOR(/)
+#undef OPERATOR
+#define OPERATOR(OPENAME) vec3 operator OPENAME(const float t, const vec3& v){return v*t;}
+    OPERATOR(*)
+#undef OPERATOR
+}
